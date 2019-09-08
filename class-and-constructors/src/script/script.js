ad3bb777573c6176964b6ddc0c86ceeb09db6a27
Vlad Vovk <wolfykey@gmail.com>
2019-09-08 20:00:14 +0300

    add new lesson folder

---
+++ class-and-constructors/src/script/script.js
@@ -0,0 +1,38 @@
+let box = document.getElementById('box'),
+    btn = document.getElementsByTagName('button'),
+    circle = document.getElementsByClassName('circle'),
+    heart = document.querySelectorAll('.heart'),
+    oneHeart = document.querySelector('.heart'),
+    wrapper = document.querySelector('.wrapper');
+
+box.style.backgroundColor = 'blue';
+btn[1].style.borderRadius = '100%';
+
+circle[0].style.backgroundColor = 'red';
+circle[1].style.backgroundColor = 'yellow';
+circle[2].style.backgroundColor = 'green';
+
+// for (let i = 0; i < heart.length; i++) {
+//     heart[i].style.backgroundColor = 'blue';
+// }
+// heart.forEach((item, i, hearts) => {
+//     item.style.backgroundColor = 'blue';
+// });
+
+let div = document.createElement('div'),
+    text = document.createTextNode('djfls');
+
+div.classList.add('black');
+
+// div.innerHTML = '<h1>Hello world</h1>';
+div.textContent = 'hello world';
+
+// document.body.appendChild(div);
+// wrapper.appendChild(div);
+document.body.insertBefore(div, circle[0]);
+document.body.removeChild(circle[1]);
+wrapper.removeChild(heart[1]);
+
+document.body.replaceChild(btn[1], circle[1]);
+
+console.log(div, text);
