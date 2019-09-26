2c52ed12163f6dfead8f83b91808f70bc6c34680
Vlad Vovk <wolfykey@gmail.com>
2019-09-26 20:05:03 +0300

    add

--- class-and-constructors/src/script/script.js
+++ class-and-constructors/src/script/script.js
@@ -35,3 +35,15 @@ let ivan = new User('Ivan', 13);
 let alex = new User('Alex', 17);
 
 ivan.exit();
+
+// this
+'use strict';
+function showThis(a, b) {
+    console.log(this);
+    function sum() {
+        console.log(this);
+        return a + b;
+    }
+    console.log(sum());
+}
+showThis(4, 5);
--- task009/js/script.js
+++ task009/js/script.js
@@ -83,4 +83,29 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     setClock('timer', deadline);
+
+    // modal
+    let more = document.querySelector('.more');
+    let descriptionBtns = document.querySelectorAll('.description-btn');
+    let overley = document.querySelector('.overlay');
+    let close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function () {
+        overley.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function () {
+        overley.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    descriptionBtns.forEach(item => {
+        item.addEventListener('click', () => {
+            overley.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        });
+    });
 });
